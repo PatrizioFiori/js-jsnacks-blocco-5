dbabcaf1368dd1b1326b308bf2d175fcbae1c818
PatrizioFiori <patrizio.fiori1995@gmail.com>
2024-12-02 14:57:23 +0100

    Snack-7 (def)

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+
+const findPerID = students.find(students => students.id === 2 )
+console.log(findPerID);
