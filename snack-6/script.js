1a356d21c8b5d9663e2098bad63feafd8829de5e
PatrizioFiori <patrizio.fiori1995@gmail.com>
2024-12-02 14:53:46 +0100

    Snack-6 (def)

--- snack-6/script.js
+++ snack-6/script.js
@@ -11,4 +11,16 @@ const zucchine = [
   { type: 'Calabrese', weight: 14, length: 4 },
 ];
 
-// A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+/* A partire dall'array fornito, crea due array. 
+Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.*/
+
+const zucchineLunghe = zucchine.filter(zucchine =>{
+  return zucchine.length > 15 ? true : false 
+})
+
+const zucchineCorte = zucchine.filter(zucchine =>{
+  return zucchine.length <= 15 ? true : false 
+})
+
+console.log(zucchineLunghe);
+console.log(zucchineCorte)
