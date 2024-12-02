5689c7164c22e27bc1bb571e4f56d87839893280
PatrizioFiori <patrizio.fiori1995@gmail.com>
2024-12-02 14:24:26 +0100

    Snack-3 (def)

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,21 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const numbersPiuUno = numbers.map(elemento => {
+    return elemento + 1   
+})
+
+console.log(numbersPiuUno);
+
+
+
+const numbersV2 = [2, 8, 4, 7, 2, 87];
+
+
+console.log("=== Versione n2 ===");
+// Return e destructuring impliciti 
+
+const numbersPiuUnoV2 = numbersV2.map (elem => elem + 1)
+
+console.log(numbersPiuUnoV2);
