05162549fd2cedf1d61ccb6df4e57d411deb480a
PatrizioFiori <patrizio.fiori1995@gmail.com>
2024-12-02 15:04:17 +0100

    Snack-8 (Def)

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+
+const findClasse = students.find(studente => studente.name === "Marco Lanci")
+console.log(findClasse.class);
